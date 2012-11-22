a85c61f32be46a5fcf4e9a722d5c57cc5df5da0c
Matt "Hayashi" Windsor <matt.windsor@ury.york.ac.uk>
2012-11-22 23:44:31 +0000

    decouples key from lass

--- metadata/models/key.py
+++ metadata/models/key.py
@@ -2,9 +2,23 @@
 key-value storage system.
 
 """
+from django.conf import settings
 from django.db import models
+
 from metadata.models import Type
-from urysite import model_extensions as exts
+
+
+METADATA_KEY_DB_TABLE = getattr(
+    settings,
+    'METADATA_KEY_DB_TABLE',
+    'metadata_key'
+)
+
+METADATA_KEY_DB_ID_COLUMN = getattr(
+    settings,
+    'METADATA_KEY_DB_ID_COLUMN',
+    'metadata_key_id'
+)
 
 
 class MetadataKey(Type):
@@ -13,15 +27,17 @@ class MetadataKey(Type):
 
     """
 
-    class Meta(Type.Meta):
-        db_table = 'metadata_key'  # in schema 'metadata'
-        app_label = 'metadata'
-
     allow_multiple = models.BooleanField(
         default=False,
         help_text="""If True, multiple instances of this metadata key
             can be active at the same time (e.g. arbitrary tags).
 
             """)
+    id = models.AutoField(
+        primary_key=True,
+        db_column=METADATA_KEY_DB_ID_COLUMN
+    )
 
-    id = exts.primary_key_from_meta(Meta)
+    class Meta(Type.Meta):
+        db_table = METADATA_KEY_DB_TABLE
+        app_label = 'metadata'
